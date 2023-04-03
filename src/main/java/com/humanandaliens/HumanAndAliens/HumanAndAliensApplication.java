1360224ecc3264596206952937b5b9c92100ec1e
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-03 15:29:08 +0200

    initial commit

---
+++ src/main/java/com/humanandaliens/HumanAndAliens/HumanAndAliensApplication.java
@@ -0,0 +1,13 @@
+package com.humanandaliens.HumanAndAliens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanAndAliensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanAndAliensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humanandaliens/HumanAndAliens/HumanAndAliensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanandaliens.HumanAndAliens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanAndAliensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

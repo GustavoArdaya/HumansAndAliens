1448baed61046177ac8ebcd75e747ee1145234a1
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-04 09:10:25 +0200

    deal and recieve damage tests

--- src/test/java/com/humanandaliens/HumanAndAliens/Entities/AlienTest.java
+++ src/test/java/com/humanandaliens/HumanAndAliens/Entities/AlienTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class AlienTest {
 
     Alien testAlien = new Alien("Test Alien");
+    Human testHuman = new Human("Test Human");
     @BeforeEach
     void setUp() {
 
@@ -17,6 +18,20 @@ class AlienTest {
     void alienShouldHave100Health() {
         var sut = testAlien.getHealth();
         assertEquals(100, sut);
+    }
+
+    @Test
+    void alienShouldReceiveDamage() {
+        testAlien.receiveDamage(20);
+        var sut = testAlien.getHealth();
+        assertEquals(80,sut);
+    }
+
+    @Test
+    void alienShouldDealDamage() {
+        testAlien.dealDamage(testHuman,20);
+        var sut = testHuman.getHealth();
+        assertEquals(80, sut);
     }
 
 
--- src/test/java/com/humanandaliens/HumanAndAliens/Entities/HumanTest.java
+++ src/test/java/com/humanandaliens/HumanAndAliens/Entities/HumanTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class HumanTest {
 
     Human testHuman = new Human("Test Human");
+    Alien testAlien = new Alien("Test Alien");
     @BeforeEach
     void setUp() {
 
@@ -17,6 +18,20 @@ class HumanTest {
     void humanShouldHave100Health() {
         var sut = testHuman.getHealth();
         assertEquals(100, sut);
+    }
+
+    @Test
+    void humanShouldReceiveDamage() {
+        testHuman.receiveDamage(20);
+        var sut = testHuman.getHealth();
+        assertEquals(80,sut);
+    }
+
+    @Test
+    void humanShouldDealDamage() {
+        testHuman.dealDamage(testAlien,20);
+        var sut = testAlien.getHealth();
+        assertEquals(80, sut);
     }
 
 }
